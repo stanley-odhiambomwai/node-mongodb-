a5763dd2b9f536a2650a8d4fe904bc5ebac783dd
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-07 12:10:25 +0300

    Connecting to MongoDB

--- server.js
+++ server.js
@@ -13,3 +13,9 @@ const {
     removeManyPeople,
     queryChain,
   } = require('./services/personService');
+
+  
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
