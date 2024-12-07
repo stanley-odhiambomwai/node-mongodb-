b62978296cc0e3bb0335965be03a0918c8c8626e
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-07 11:50:59 +0300

    Function to find a person by ID

--- Service/personService.js
+++ Service/personService.js
@@ -37,4 +37,12 @@ const findOneByFood = (food, done) => {
       done(null, person);
     });
   };
+
+  
+const findPersonById = (personId, done) => {
+    Person.findById(personId, (err, person) => {
+      if (err) return console.error(err);
+      done(null, person);
+    });
+  };
   
