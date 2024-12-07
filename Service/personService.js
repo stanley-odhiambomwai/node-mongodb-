98d7dee808f6153a7614eb057cb752790fe4f99d
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-07 11:43:11 +0300

    a function to creat a person and save it

--- Service/personService.js
+++ Service/personService.js
@@ -1,1 +1,16 @@
 const Person = require('../models/personModel');
+
+
+const createAndSavePerson = (done) => {
+    const person = new Person({
+      name: 'John Doe',
+      age: 30,
+      favoriteFoods: ['Pizza', 'Pasta'],
+    });
+  
+    person.save((err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  };
+  
