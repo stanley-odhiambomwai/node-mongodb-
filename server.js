ef9d293437029b7ec98aee0bd2dd3357f67ccba3
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-07 12:08:35 +0300

    importing all the functions from serices folder

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const {
+    createAndSavePerson,
+    createManyPeople,
+    findPeopleByName,
+    findOneByFood,
+    findPersonById,
+    findEditThenSave,
+    findAndUpdate,
+    removeById,
+    removeManyPeople,
+    queryChain,
+  } = require('./services/personService');
